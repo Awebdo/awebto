8e9a0a426d65c44a725cc117c198dcf9d5b5d8fa
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-09-02 22:45:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -332,21 +332,6 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === ("Monopoly")) {
-	    message.reply(":thinking:")
-	    message.author.send("Easter Egg 7/10\nI forgot my version can you please tell me the MAIN version?")
-	    message.delete(100)
-    }
-});
-client.on('message', message => {
-    if (message.content === ("1.0.0")) {
-	    message.reply(":thinking:")
-	    message.author.send("Easter Egg 8/10\nWhats the 4th rule in the SERVER rules?")
-	    message.delete(100)
-    }
-});
-
-client.on('message', message => {
     if (message.content.includes("Dont suggest useless ideas or ideas that already exist")) {
 	    message.reply(":thinking:")
 	    message.author.send("Easter Egg 9/10\nDamn! i think my caps lock was broken, can you tell what i said in capitals in the previous easter eggs in a sentence?")
