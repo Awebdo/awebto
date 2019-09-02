339167530f7f2e70a54388a68ee6cb4d131fa1ee
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-09-02 23:23:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -330,5 +330,34 @@ client.on('message', msg => {
     });
   }
 });
+client.on('message', message => {
+	if (command === "sendguildmessages") {
+  if (message.author.id === "231956829159161856") {
+    try {
+      let toSay = "messageToSend"
+      this.client.guilds.map((guild) => {
+        let found = 0
+        guild.channels.map((c) => {
+          if (found === 0) {
+            if (c.type === "text") {
+              if (c.permissionsFor(this.client.user).has("VIEW_CHANNEL") === true) {
+                if (c.permissionsFor(this.client.user).has("SEND_MESSAGES") === true) {
+                  c.send(toSay);
+                  found = 1;
+                }
+              }
+            }
+          }
+        });
+      });
+    }
+    catch (err) {
+      console.log("Could not send message to a (few) guild(s)!");
+    }
+  } else {
+    message.reply("You cant do that!")
+  }
+}
+});
 
 client.login(process.env.BOT_TOKEN);
