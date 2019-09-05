eb10b64cd96f08a0db03404cccd78289b28d5eda
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-09-05 16:50:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -239,7 +239,7 @@ client.on('message', message => {
      if (message.content.includes("shit") || message.content.includes("خخخ") || message.content.includes("fuck") || message.content.includes("احا") || message.content.includes("كسم") || message.content.includes("شرموط") || message.content.includes("متناك") || message.content.includes("kosom") || message.content.includes("a7a") || message.content.includes("ksm") || message.content.includes("asshole") || message.content.includes("cunt") || message.content.includes("bitch") || message.content.includes("mtnak")) {
 	     if (message.author.bot) return;
 	     if (message.member.hasPermissions("MANAGE_MESSAGES")) return;
-       message.delete(100)
+       message.delete(1000)
                         
        message.reply("No swearing allowed, Swear deleted :slight_smile:")
        }
@@ -251,7 +251,7 @@ client.on('message', message => {
     var text = message.content.split(' ').slice(1).join (' ')
     if (!text) return message.reply('Say what? I didnt hear you.')
     message.channel.send(text)
-    message.delete(100)
+    message.delete(1000)
    }
 });
 
