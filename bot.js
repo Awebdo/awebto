edf0a4e84c48131663c116b914bd9e576f8427a8
Awebdo <49924103+Awebdo@users.noreply.github.com>
2019-09-12 19:30:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,37 +6,6 @@ const prefix = "-";
 
  
 
-client.on('ready', () => {
-
-    console.log('I am ready!')
-client.user.setPresence({ game: { name: 'with discord.js' }, status: 'idle' })
- .then(console.log)
- .catch(console.error);
-
-});
-
- client.on("message", message => {
-   var embedfoo = new Discord.RichEmbed()
-   .setAuthor('Twoon BOT', 'https://code.fb.com/wp-content/uploads/2017/06/Facebook-Messenger-Bot-01.png')		
-   .setTitle('Echo!')		
-   .setColor('#0099ff')		
-   .setDescription(message.content.replace('-echoembed', ''))		
-   .setTimestamp()		
-   .setFooter('Twoon bot programmed by Youssef & Awebdo')
-  if (message.content.startsWith("-echoembed")) {		
-   if (message.member.hasPermissions("MANAGE_MESSAGES")) {
-   message.delete(1000)		
-  message.channel.send({embed: embedfoo});		
-
- } else {
-  message.channel.send("Sorry mate, but you are not allowed to use this command :sob:")
-  }
-   
- 
-   				}			
-
-   });		
-
     client.on("message", message => {
 	      if (message.content.startsWith(prefix + "verify")) {
 		      if (message.channel.name !== 'verification') return;
@@ -63,65 +32,5 @@ message.channel.send(`${message.author.username} was successfully verified :whit
 		      }
 		      }
 	    })
-       
  
-
-
-client.on("message", message=> {
-    if (message.content === (prefix + "clear")) {
-if (message.member.hasPermissions("MANAGE_MESSAGES")) {
-     message.channel.bulkDelete(10);
-message.channel.send(" 10 messages successfully deleted :white_check_mark:");
-message.channel.bulkDelete(1);
-} else {
-message.reply("you do not have permissions to use this command")
-}
-    }
-});
- 
-client.on('message', message => {
-    if(message.content.startsWith(prefix + "ping")) {
-            message.channel.send('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-       }
-});
-
-
-client.on('message', message => {
-  
-     if (message.content.includes("***") || message.content.includes("خخخ") || message.content.includes("***") || message.content.includes("احا") || message.content.includes("كسم") || message.content.includes("شرموط") || message.content.includes("متناك") || message.content.includes("kosom") || message.content.includes("a7a") || message.content.includes("ksm") || message.content.includes("***") || message.content.includes("***") || message.content.includes("bitch") || message.content.includes("mtnak")) {
-	     if (message.author.bot) return;
-	     if (message.member.hasPermissions("MANAGE_MESSAGES")) return;
-       message.delete(1000)
-                        
-       message.reply("No swearing allowed, Swear deleted :slight_smile:")
-       }
-});
-
-
-client.on('message', message => {
-   if (message.content.startsWith(prefix + "say")) {
-    var text = message.content.split(' ').slice(1).join (' ')
-    if (!text) return message.reply('Say what? I didnt hear you.')
-    message.channel.send(text)
-    message.delete(1000)
-   }
-});
-
-
-
-client.on('message', msg => {
-  if (msg.guild && msg.content.startsWith(prefix + "broadcast")) {
-    let text = msg.content.slice('-broadcast'.length); // cuts off the /private part
-    msg.guild.members.forEach(member => {
-      if (member.id != client.user.id && !member.user.bot) member.send(text);
-    });
-  }
-});
-
-client.on('message', message => {
-    if (message.content.includes('discord.gg')) {
-    message.channel.send("No advertising allowed");
-    message.delete(100)
-    }
-});
 client.login(process.env.BOT_TOKEN);
